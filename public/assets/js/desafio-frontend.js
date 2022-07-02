8f922e87d2023db5424bcb13e91cd4b98b777724
Elizeu <elizeu_os@hotmail.com>
2022-07-02 11:36:01 -0300

    Organize code and add coments to the file

--- public/assets/js/desafio-frontend.js
+++ public/assets/js/desafio-frontend.js
@@ -1,40 +1,50 @@
+//Get API data
 function getAPIURL(url) {
   let request = new XMLHttpRequest();
+
   request.open("GET", url, false);
   request.send();
+
   return request.responseText;
 }
 
-function renderNavigationsWithCategories(categories) {
+//Consume Mock-API categorie list data
+function getMockAPICategoryList() {
+  let data = getAPIURL("http://localhost:8888/api/V1/categories/list");
+
+  return JSON.parse(data).items;
+}
+
+//Render page link items and categories items
+function renderNavigationsWithCategories() {
+  let categories = getMockAPICategoryList();
+
   let elements = document.querySelectorAll(".js-render-nav-categories");
 
-  if (null === elements) {
+  if (null === categories || null === elements) {
     return;
   }
 
-  elements.forEach((element)=>{
+  elements.forEach((element) => {
     let result = '<li><a href="/">Página inicial</a> </li>';
 
     categories.forEach((category) => {
       result +=
-        '<li><a href="/categoria/' +
+        '<li><a href="?categoria=' +
         category.path +
         '">' +
         category.name +
         "</a></li>";
-      console.log(result);
     });
-  
+
     result += '<li><a href="">Contato</a></li>';
-  
     element.innerHTML = result;
   });
 }
 
+//Start main functions
 function init() {
-  let data = getAPIURL("http://localhost:8888/api/V1/categories/list");
-  let categories = JSON.parse(data).items;
-  renderNavigationsWithCategories(categories);
+  renderNavigationsWithCategories();
 }
 
 init();
